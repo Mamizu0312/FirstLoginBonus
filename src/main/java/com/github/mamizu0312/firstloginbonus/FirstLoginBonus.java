dc6b4207290184b29633fcd8a8a4da938c1ca48e
BuildTools <unconfigured@null.spigotmc.org>
2018-08-20 15:51:54 +0900

    v1.3 よりゴミみたいなミスを修正

--- src/main/java/com/github/mamizu0312/firstloginbonus/FirstLoginBonus.java
+++ src/main/java/com/github/mamizu0312/firstloginbonus/FirstLoginBonus.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -57,7 +58,7 @@ public final class FirstLoginBonus extends JavaPlugin implements Listener {
 
         return false;
     }
-
+    @EventHandler
     public void onPlayerFirstLogin(PlayerJoinEvent e) {
         Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
             Player p = e.getPlayer();
